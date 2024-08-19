94194bca5900d4de21bf8e690a0fd15e7e7da00e
alesl10 <lopezalexis499@gmail.com>
2024-08-19 08:34:20 -0300

    clone

--- ProyectoPrueba.Repositorio/obj/Debug/net8.0/ProyectoPrueba.Repositorio.AssemblyInfo.cs
+++ ProyectoPrueba.Repositorio/obj/Debug/net8.0/ProyectoPrueba.Repositorio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoPrueba.Repositorio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+add03876fa995a1e631bb80634ce7b7977d33e81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf3e29ee43f2b38bf36e57a8e57d48288ccf09d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoPrueba.Repositorio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoPrueba.Repositorio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
